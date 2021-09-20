d1bc1f1612f54476f15caf3358160dfd24f91228
PUSHkin42342443 <alexandderdudarev@gmail.com>
2021-09-20 19:28:22 +0500

    Добавьте файлы проекта.

---
+++ ПУШkin/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ПУШkin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("привет, синий , я зелёный");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("привет, зелёный, как дела?");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("короче, меченый, я тебя спас и вблагородство играть не буду, выполнишь для меня пау заданий и мы врасчёте, заодно посмотрим как быстро у тебя башка после амнезии прояснится, а по твоей теме: постараюсь разузнать, хрен его знает на кой ляг тебе этот стерлок сдался, но я вчужие дела не лезу");
+        }
+    }
+}
